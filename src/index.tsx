3aa072bbc725dbb33c34d6dbe7f27feaaa3a19f5
daniel martins <freitasdaniel39@gmail.com>
2021-09-25 13:27:41 -0300

    adding miragejs in the project

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        { id: 1,
+          title: 'Transaction 1',
+          amount: 480,
+          type: 'deposit',
+          category: 'Food',
+          date: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
